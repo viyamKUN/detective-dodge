5b530bd4bb4e1f00c3dd908caace5ec94628d179
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-02-17 22:32:55 +0900

    플레이어 데이터 구조 구현 및 연결

---
+++ Assets/Scripts/DefaultSystem/SaveDataManager.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace DefaultSystem
+{
+    using Data.VariableData;
+    public static class PlayerSaveData
+    {
+        private readonly static string _saveFileName = $"{Application.persistentDataPath}/savedata.save";
+        private static Player _player;
+        public static Player GetPlayer => _player;
+
+        public static void Init(string name, int storyCount)
+        {
+            _player = new Player(name, storyCount);
+        }
+
+        public static void Save()
+        {
+            using var file = File.Create(_saveFileName);
+            new BinaryFormatter().Serialize(file, _player);
+        }
+
+        /// <summary>
+        /// 게임 데이터가 있으면 Load 합니다.
+        /// </summary>
+        public static bool Load()
+        {
+            if (File.Exists(_saveFileName))
+            {
+                using var file = File.Open(_saveFileName, FileMode.Open);
+                if (file != null && file.Length > 0)
+                {
+                    _player = (Player)new BinaryFormatter().Deserialize(file);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void Delete()
+        {
+            File.Delete(_saveFileName);
+        }
+    }
+}
--- Assets/Scripts/Home/HomeManager.cs
+++ Assets/Scripts/Home/HomeManager.cs
@@ -6,21 +6,40 @@ using DG.Tweening;
 namespace Home
 {
     using DefaultUI;
+    using DefaultSystem;
+
     public class HomeManager : MonoBehaviour
     {
         [SerializeField]
         private SceneLoader _loader;
         [SerializeField]
         private StoriesButtonsManager _storyButtonsManager;
+        [SerializeField]
+        private UpperUIManager _upperUIManager;
 
         private void Awake()
         {
             // 스토리 데이터 리딩 (한 번만)
+            if (!PlayerSaveData.Load())
+            {
+                // 이름 입력 이벤트 출력
+                EnterPlayerName("김호걸");
+            }
             // 진행도 표시
             _storyButtonsManager.Init();
+            _upperUIManager.SetUI(PlayerSaveData.GetPlayer.Name, PlayerSaveData.GetPlayer.EXP);
             DOTween.Init(true, true, LogBehaviour.Default);
         }
 
+        public void EnterPlayerName(string name)
+        {
+            int storyCount = 7;
+            PlayerSaveData.Init(name, storyCount);
+        }
+
+        /// <summary>
+        /// 게임 시작 버튼을 통해서 입력받습니다.
+        /// </summary>
         public void GameStart()
         {
             _loader.Load(SceneName.Game);
