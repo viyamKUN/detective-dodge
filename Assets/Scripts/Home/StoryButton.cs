068e3685f6f20bd318fb6355b0f9b37441cf2872
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-02-19 09:54:49 +0900

    락 되어있는 스토리는 잠금

--- Assets/Scripts/Home/StoryButton.cs
+++ Assets/Scripts/Home/StoryButton.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Home
 {
     public class StoryButton : MonoBehaviour
     {
+        [SerializeField]
+        private RectTransform _rect;
         [SerializeField]
         private DefaultUI.ButtonAnimator _buttonAnimator;
         [SerializeField]
@@ -16,9 +18,11 @@ namespace Home
         private GameObject _notReadToken;
         private HomeManager _manager;
         private int _scenarioNumber;
+        private Vector2 _originalPos;
         public void Init(int number, HomeManager manager)
         {
             _buttonAnimator.Init(Click);
+            _originalPos = _rect.anchoredPosition;
             _scenarioNumber = number;
             _manager = manager;
             bool isLock = !DefaultSystem.PlayerSaveData.GetPlayer.ActivateStories[number].IsActivate;
@@ -29,6 +33,12 @@ namespace Home
 
         private void Click()
         {
+            bool isLock = !DefaultSystem.PlayerSaveData.GetPlayer.ActivateStories[_scenarioNumber].IsActivate;
+            if (isLock)
+            {
+                _rect.DOShakeAnchorPos(0.5f, 60).OnComplete(() => _rect.anchoredPosition = _originalPos);
+                return;
+            }
             Data.StaticData.StoryBookmark.SetScenarioNumber(_scenarioNumber);
             _manager.StoryStart(_scenarioNumber);
         }
